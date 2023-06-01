1377dd7661879dfdcfb5ef5d35b090879fda8a0b
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-06-01 20:26:42 +0300

    ASP.NET-CORE-MVC-CRUD-ENTITY-FRAMEWORK

--- ASPNETMVCCRUD/Program.cs
+++ ASPNETMVCCRUD/Program.cs
@@ -1,7 +1,11 @@
+using ASPNETMVCCRUD.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MVCDemoDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
