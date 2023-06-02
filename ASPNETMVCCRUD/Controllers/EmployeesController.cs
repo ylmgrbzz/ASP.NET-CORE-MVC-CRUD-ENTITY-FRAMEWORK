27cd641605ea3a67e32956405c62ee025c9107f2
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-06-02 16:32:28 +0300

    ASP.NET-CORE-MVC-CRUD-ENTITY-FRAMEWORK

--- ASPNETMVCCRUD/Controllers/EmployeesController.cs
+++ ASPNETMVCCRUD/Controllers/EmployeesController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASPNETMVCCRUD.Data;
+using ASPNETMVCCRUD.Models;
+using ASPNETMVCCRUD.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASPNETMVCCRUD.Controllers
 {
     public class EmployeesController : Controller
     {
+        public readonly MVCDemoDbContext mvcDemoContext;
+
+        public EmployeesController(MVCDemoDbContext mvcDemoContext)
+        {
+            this.mvcDemoContext = mvcDemoContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                DateOfBirth = addEmployeeRequest.DateOfBirth,
+                Department = addEmployeeRequest.Department
+            };
+
+            mvcDemoContext.Employees.Add(employee);
+            mvcDemoContext.SaveChanges();
             return View();
         }
     }
